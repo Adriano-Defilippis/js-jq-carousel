e2dff0f18a8ce5326dbdb073a84d85b122ed0ce8
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-19 17:28:58 +0200

    Lo slideShow è completo va avanti e dietro

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//ESECUZIONE DELLO SCRIPT DOPO
+//IL CARICAMENTO DELLA PAGINA
 $(document).ready(function(){
 
   //Setto una variabile con selettore JQuery per salvare il dom dei pulsanti dello slider
@@ -7,21 +9,37 @@ $(document).ready(function(){
   //Creo l'evento click sui controlli delle slide
   //Azione per scrorrere in avanti
   next.click(nextSlide);
-
-
-
+  prev.click(prevSlide);
 
 });
 
 
-//Creo le funzioni per le azioni del click sui pulsanti per scrorre le immagini in un carousel
-//Funzione per andare avanti
+
+
+//FUNZIONI DEL MIO SCRIPT
+
+//Funzione per le azioni del click sui pulsanti per scrorre le immagini in un carousel
 function nextSlide(){
   var img = $('img.active');
 
   if (img.hasClass("end")) {
     img.removeClass("active");
-    $('.start').addClass('active');
+    $('img.start').addClass('active');
+
+  }else{
+    img.removeClass("active");
+    img.next("img").addClass("active");
+  }
+
+};
+
+//Funzione per mostrare Slide Precedente
+function prevSlide(){
+  var img = $('img.active');
+
+  if (img.hasClass("start")) {
+    img.removeClass("active");
+    $('img.end').addClass('active');
 
   }else{
     img.removeClass("active");
