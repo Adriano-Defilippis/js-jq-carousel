55726801deaaa312ca301c49b02b23de8a91c4ba
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-19 17:24:06 +0200

    Pulizia del codice

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
+$(document).ready(function(){
+
+  //Setto una variabile con selettore JQuery per salvare il dom dei pulsanti dello slider
+  var prev = $('.prev');
+  var next = $('.next');
+
+  //Creo l'evento click sui controlli delle slide
+  //Azione per scrorrere in avanti
+  next.click(nextSlide);
+
+
+
+
+});
+
+
 //Creo le funzioni per le azioni del click sui pulsanti per scrorre le immagini in un carousel
-
-//Funzione per andare avanti
-function nextSlide(imgDom){
-
-
-
-};
-
 //Funzione per andare avanti
 function nextSlide(){
   var img = $('img.active');
@@ -18,27 +26,6 @@ function nextSlide(){
   }else{
     img.removeClass("active");
     img.next("img").addClass("active");
-
   }
 
-
-  console.log(img.next("img"));
-
 };
-
-
-
-
-
-
-
-
-//Creo l'evento click sui controlli delle slide
-
-//Setto una variabile con selettore JQuery per salvare il dom dei pulsanti dello slider
-var prev = $('.prev');
-var next = $('.next');
-var imgActive = $('img.active');
-
-//Azione per scrorrere in avanti
-next.click(nextSlide);
